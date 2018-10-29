77798ca8211da836b178048846b0ca7a94a26f82
Ned Ruggeri <ruggeri@self-loop.com>
2018-10-29 14:15:05 -0700

    reviewer can now color active task

--- src/reviewer.rs
+++ src/reviewer.rs
@@ -1,23 +1,33 @@
 use super::models::Task;
 
 pub struct Reviewer {
+  current_task_idx: usize,
   tasks: Vec<Task>,
 }
 
 impl Reviewer {
   pub fn new(tasks: Vec<Task>) -> Reviewer {
-    Reviewer { tasks }
+    Reviewer { current_task_idx: 0, tasks }
   }
 
   pub fn print(&self) {
-    for task in &self.tasks {
+    for (idx, ref task) in self.tasks.iter().enumerate() {
+      if idx == self.current_task_idx {
+        print!("\x1b[42m")
+      }
+
       let s = format!(
         "{id:4} | {title:50} | {created_at}",
         id = task.id,
         title = task.title,
         created_at = task.created_at
       );
+
       println!("{}", s);
+
+      if idx == self.current_task_idx {
+        print!("\x1b[49m")
+      }
     }
   }
 }
