2aaae2be71b0ee253229bb202d35b2bbd6542a41
Ned Ruggeri <ruggeri@self-loop.com>
2018-11-03 18:15:54 -0700

    fix problem where tm won't run when pf drops all packets

--- src/util/connection.rs
+++ src/util/connection.rs
@@ -2,8 +2,10 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 
 pub fn get_connection() -> PgConnection {
-  // TODO: Break this out into a configuration file I guess.
-  let database_url = "postgres://ruggeri@localhost:5432/task_manager";
+  // TODO: Break this out into a configuration file I guess. Look how I
+  // don't specify even localhost. By doing so, I will use a Unix domain
+  // socket, which means I won't be blocked by my firewall.
+  let database_url = "postgres://ruggeri@:5432/task_manager";
   PgConnection::establish(&database_url).unwrap_or_else(|_| {
     panic!("Error connecting to {}", database_url);
   })
