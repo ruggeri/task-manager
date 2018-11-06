5369f5dcb1af7ac580212386f6c1a99b210ef54e
Ned Ruggeri <ruggeri@self-loop.com>
2018-11-06 14:53:09 -0800

    get ready for Views restructuring

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,11 @@ extern crate pancurses;
 extern crate rustyline;
 
 pub mod actions;
+pub mod application;
 pub mod commands;
 pub mod components;
 pub mod models;
 pub mod queries;
 pub mod schema;
 pub mod util;
+pub mod views;
